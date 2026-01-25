42e2dd1945223c2fc2c747260cb6ba08cb68e1bf
Dmitry Ryaboshapko <ryaboshapkodi@sovcombank.ru>
2026-01-25 19:09:00 +0300

    Выполненое задание по FLow

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,7 +1,13 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -10,22 +16,40 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsStateFlow = MutableStateFlow<Result>(Result.Success(Unit))
+    val catsStateFlow = _catsStateFlow.asStateFlow()
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                catsRepository.listenForCatFacts()
+                    .catch { error ->
+                        _catsStateFlow.emit(
+                            Result.Error(
+                                error.message.orEmpty(),
+                            ),
+                        )
+                    }
+                    .collect { fact ->
+                    _catsStateFlow.emit(
+                        Result.Success(fact),
+                    )
                 }
             }
         }
     }
 }
 
+sealed interface Result {
+    data class Success<T>(val result: T) : Result
+    @JvmInline
+    value class Error(val message: String) : Result
+}
+
+@Suppress("UNCHECKED_CAST")
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-        CatsViewModel(catsRepository) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CatsViewModel(catsRepository) as T
+    }
 }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -2,20 +2,41 @@ package otus.homework.flowcats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private val diContainer = DiContainer()
     private val catsViewModel by viewModels<CatsViewModel> { CatsViewModelFactory(diContainer.repository) }
 
+    @OptIn(InternalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launchWhenStarted {
+            launch {
+                catsViewModel.catsStateFlow.collect { result ->
+                    when (result) {
+                        is Result.Success<*> -> {
+                            val fact = result.result as? Fact
+                            if (fact != null) {
+                                view.populate(result.result)
+                            }
+                        }
+
+                        is Result.Error -> {
+                            Log.d("Error", result.message)
+                        }
+                    }
+                }
+            }
         }
     }
 }
